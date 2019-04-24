97cf84d753efb781a52d54eb3a99d80284359a4c
huangmin <hm-secret@163.com>
2019-04-24 16:40:35 +0800

    Update RaspiTest.sh

--- rpi_shell/RaspiTest.sh
+++ rpi_shell/RaspiTest.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 #filename RaspiTest
 
+#update this bash
 git pull origin master
 
+#test echo
 echo "Hello"
+
+#test echo
+echo "World"
+
+#test
 _IP=$(hostname -I) || true
 echo $_IP
 
