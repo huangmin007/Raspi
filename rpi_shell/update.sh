f75bbe20ae948a0fa68c5771482058608267b78e
huangmin <hm-secret@163.com>
2019-04-24 18:04:12 +0800

    Update update.sh

--- rpi_shell/update.sh
+++ rpi_shell/update.sh
@@ -2,8 +2,14 @@
 #filename update
 
 #update startup.sh bash
-git checkout startup.sh
-git pull origin master
+ping github.com -c 1 > /dev/null 2 > &1
+if [ $? -eq 0 ]; then
+    echo "Network Connection OK!"
+    git checkout startup.sh
+    git pull origin master
+else
+    echo "Network Connection Error!"
+fi
 
 echo "ready startup ..."
 sudo chmod 777 startup.sh
