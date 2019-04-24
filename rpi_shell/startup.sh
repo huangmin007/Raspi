f752afeb1beaf086e20c7825c899bb92dc8b1bf4
huangmin <hm-secret@163.com>
2019-04-24 16:47:06 +0800

    Create startup.sh

---
+++ rpi_shell/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#filename RaspiTest
+
+#update this bash
+git pull origin master
+
+#test echo
+echo "Hello"
+
+#test echo
+echo "World"
+
+#test
+_IP=$(hostname -I) || true
+echo $_IP
